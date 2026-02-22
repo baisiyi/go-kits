c4b638ce6db98b1b2e489f1b12b1218f6b1cb723
baisiyi <2455138283@qq.com>
2026-02-22 20:38:25 +0800

    补充信息

--- plugin/setup.go
+++ plugin/setup.go
@@ -1,6 +1,23 @@
 package plugin
 
-import "gopkg.in/yaml.v3"
+import (
+	"errors"
+
+	"gopkg.in/yaml.v3"
+)
 
 // Config is the configuration of all plugins. plugin type => { plugin name => plugin config }
 type Config map[string]map[string]yaml.Node
+
+// YamlNodeDecoder is a decoder for a yaml.Node of the yaml config file.
+type YamlNodeDecoder struct {
+	Node *yaml.Node
+}
+
+// Decode decodes a yaml.Node of the yaml config file.
+func (d *YamlNodeDecoder) Decode(cfg interface{}) error {
+	if d.Node == nil {
+		return errors.New("yaml node empty")
+	}
+	return d.Node.Decode(cfg)
+}
